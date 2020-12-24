559d27f03e30531742781de9e2731b6e5c1dcd01
neelimayegireddy <70631395+neelimayegireddy@users.noreply.github.com>
2020-12-24 18:47:04 +0530

    Update JDBC1.java

--- practice/src/test/java/com/practice/JDBC1.java
+++ practice/src/test/java/com/practice/JDBC1.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 import org.testng.annotations.Test;
 
 public class JDBC1 {
-	
+
     @Test
 	public void Execution() throws Exception
 	{
@@ -23,7 +23,7 @@ public class JDBC1 {
 		stmt.close();
 		con.close();
 		
-		
+		aa
 	}
 
 }
