16b8d34c4c6fec64b987d64b3b0f2dbbdf2eb392
ADMIN4 <ADMIN4@CSSLTP16>
2020-12-24 18:42:15 +0530

    JDBC1 execution

---
+++ practice/src/test/java/com/practice/JDBC1.java
@@ -0,0 +1,29 @@
+package com.practice;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.testng.annotations.Test;
+
+public class JDBC1 {
+	
+    @Test
+	public void Execution() throws Exception
+	{
+		Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/automation", "root", "root");
+		Statement stmt=con.createStatement();
+		ResultSet rs=stmt.executeQuery("select details.name,details.role,company.cname,company.salary from details RIGHT JOIN  company ON details.id=company.id;");
+		while(rs.next())
+		{
+			System.out.println(rs.getString("Name") +" " + rs.getString("Role") + " " + rs.getString("cname") + " " + rs.getString("salary"));
+		}
+		rs.close();
+		stmt.close();
+		con.close();
+		
+		
+	}
+
+}
